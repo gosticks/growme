d0ce2073f8a151e5efe21e91c71fd4e3c0d71a72
Wlad Meixner <hi@iamwlad.com>
2022-09-26 16:13:12 +0200

    chore: run formatter

--- growme/src/main.cpp
+++ growme/src/main.cpp
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <Arduino.h>
+#include <WiFi.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <WiFi.h>
-#include <Arduino.h>
+#include <stdio.h>
 
-#include "sdkconfig.h"
 #include "A4988.h"
+#include "sdkconfig.h"
 
 #define UP_BTN_PIN 27
 #define DOWN_BTN_PIN 13
@@ -40,106 +40,88 @@ int direction = 0;
 
 // TODO: for now use simple arduino based
 // task structure adjust as needed afterwards
-void motorTask(void *pvParameter)
-{
-    A4988 stepper = *(A4988 *)pvParameter;
+void motorTask(void *pvParameter) {
+	A4988 stepper = *(A4988 *)pvParameter;
 
-    // configure motor
-    stepper.begin(120, 1);
+	// configure motor
+	stepper.begin(120, 1);
 
-    // track current movement of motor
-    uint motorWaitTimeMicros = 0;
+	// track current movement of motor
+	uint motorWaitTimeMicros = 0;
 
-    // time for RTOS to use when motor is not spinning
-    const TickType_t noActionIdleTime = pdMS_TO_TICKS(100);
+	// time for RTOS to use when motor is not spinning
+	const TickType_t noActionIdleTime = pdMS_TO_TICKS(100);
 
-    // pick large angle to rotate, such that motor will not stop between loop iterations
-    // and RTOS has time to do other things
-    int rotationAngle = 100 * 360;
+	// pick large angle to rotate, such that motor will not stop between loop iterations
+	// and RTOS has time to do other things
+	int rotationAngle = 100 * 360;
 
-    // add a delay for the MCU to apply setup
-    delay(100);
+	// add a delay for the MCU to apply setup
+	delay(100);
 
-    while (1)
-    {
+	while (1) {
+		// start spinning motor if:
+		// - button pressed
+		// - no spin command was already started
+		if (direction == 0) {
+			stepper.stop();
+		} else {
+			stepper.startRotate(direction * rotationAngle);
+		}
 
-        // start spinning motor if:
-        // - button pressed
-        // - no spin command was already started
-        if (direction == 0)
-        {
-            stepper.stop();
-        }
-        else
-        {
-            stepper.startRotate(direction * rotationAngle);
-        }
-
-        // motor control loop - send pulse and return how long to wait until next pulse
-        motorWaitTimeMicros = stepper.nextAction();
-        if (motorWaitTimeMicros <= 0)
-        {
-
-            vTaskDelay(noActionIdleTime);
-        }
-        else if (motorWaitTimeMicros >= 50)
-        {
-            vTaskDelay(pdMS_TO_TICKS(motorWaitTimeMicros / 1000));
-        }
-        else
-        {
-            // give tiny amount for RTOS scheduling
-            vTaskDelay(20);
-        }
-    }
+		// motor control loop - send pulse and return how long to wait until next pulse
+		motorWaitTimeMicros = stepper.nextAction();
+		if (motorWaitTimeMicros <= 0) {
+			vTaskDelay(noActionIdleTime);
+		} else if (motorWaitTimeMicros >= 50) {
+			vTaskDelay(pdMS_TO_TICKS(motorWaitTimeMicros / 1000));
+		} else {
+			// give tiny amount for RTOS scheduling
+			vTaskDelay(20);
+		}
+	}
 }
 
-void controlTask(void *pvParameter)
-{
-    const TickType_t loopDelay = pdMS_TO_TICKS(50);
+void controlTask(void *pvParameter) {
+	const TickType_t loopDelay = pdMS_TO_TICKS(50);
 
-    // configure button
-    pinMode(UP_BTN_PIN, INPUT);
-    pinMode(DOWN_BTN_PIN, INPUT);
+	// configure button
+	pinMode(UP_BTN_PIN, INPUT);
+	pinMode(DOWN_BTN_PIN, INPUT);
 
-    while (1)
-    {
-        // parse movement direction
-        if (digitalRead(UP_BTN_PIN) == HIGH)
-        {
-            direction = 1;
-        }
-        else if (digitalRead(DOWN_BTN_PIN) == HIGH)
-        {
-            direction = -1;
-        }
-        else
-        {
-            direction = 0;
-        }
+	while (1) {
+		// parse movement direction
+		if (digitalRead(UP_BTN_PIN) == HIGH) {
+			direction = 1;
+		} else if (digitalRead(DOWN_BTN_PIN) == HIGH) {
+			direction = -1;
+		} else {
+			direction = 0;
+		}
 
-        vTaskDelay(loopDelay);
-    }
+		vTaskDelay(loopDelay);
+	}
 }
 
-extern "C" void app_main()
-{
-    // initialize arduino library before we start the tasks
-    initArduino();
+extern "C" void app_main() {
+	// initialize arduino library before we start the tasks
+	initArduino();
 
-    // setup serial COMs
-    Serial.begin(115200);
+	// setup serial COMs
+	Serial.begin(115200);
+	// bootstrap bluetooth controller
+	Bluetooth
 
-    // setup event group
-    xMotorEventGroup = xEventGroupCreate();
+		// setup event group
+		xMotorEventGroup = xEventGroupCreate();
 
-    // xTaskCreate(&arduinoTask, "arduino_task", 8192, NULL, 5, NULL);
-    xTaskCreate(&controlTask, "control_task", 8192, NULL, 3, NULL);
+	// xTaskCreate(&arduinoTask, "arduino_task", 8192, NULL, 5, NULL);
+	xTaskCreate(&controlTask, "control_task", 8192, NULL, 3, NULL);
 
-    xTaskCreate(&motorTask, "motor_1_task", 4096, (void *)&stepper1, 2, NULL);
-    // xTaskCreate(&motorTask, "motor_2_task", NULL, (void *)&stepper2, 3, NULL);
-    // xTaskCreate(&motorTask, "motor_3_task", NULL, (void *)&stepper3, 3, NULL);
-    // xTaskCreate(&motorTask, "motor_4_task", NULL, (void *)&stepper4, 3, NULL);
-    // xTaskCreate(&motorTask, "motor_5_task", NULL, (void *)&stepper5, 3, NULL);
-    // xTaskCreate(&motorTask, "motor_6_task", NULL, (void *)&stepper6, 3, NULL);
+	xTaskCreate(&motorTask, "motor_1_task", 4096, (void *)&stepper1, 2, NULL);
+	// xTaskCreate(&motorTask, "motor_2_task", NULL, (void *)&stepper2, 3, NULL);
+	// xTaskCreate(&motorTask, "motor_3_task", NULL, (void *)&stepper3, 3, NULL);
+	// xTaskCreate(&motorTask, "motor_4_task", NULL, (void *)&stepper4, 3, NULL);
+	// xTaskCreate(&motorTask, "motor_5_task", NULL, (void *)&stepper5, 3, NULL);
+	// xTaskCreate(&motorTask, "motor_6_task", NULL, (void *)&stepper6, 3, NULL);
 }
